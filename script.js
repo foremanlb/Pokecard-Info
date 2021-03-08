11a7ae1b80fe423630c49b4407e4eec3d1604c52
Craig Navarro <craignavarro@Craigs-MacBook-Pro.local>
2021-03-08 09:54:19 -0800

    remove function works

--- script.js
+++ script.js
@@ -2,13 +2,15 @@ const setSearchURL = 'https://api.pokemontcg.io/v2/sets'
 const pokemonName = 'charizard'
 const cardSearchURL = `https://api.pokemontcg.io/v2/cards?q=name:`
 const code = '81ef3d57-7b3c-4e91-9eb6-81cb3adbfb06'
-
+const nav = document.querySelector('nav')
 
 async function getPokemon() {
   try {
     const pokemon = await axios.get(`${cardSearchURL}${pokemonName}` ,
       {
-      'X-Api-Key': `${code}`
+        headers: {
+          'X-Api-Key': `${code}`
+        }
     })
     console.log(pokemon.data)
   } catch (error) {
@@ -28,4 +30,15 @@ async function getSet() {
   }
 }
 
-getSet()
+function searchGenerate() {
+
+}
+
+function removeInput() {
+  const navList = nav.childNodes
+  console.log(navList)
+  while (navList.length > 4) {
+    nav.removeChild(nav.lastChild)
+  }
+  console.log(navList)
+}
