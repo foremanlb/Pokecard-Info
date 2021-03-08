a176a6754601f272a0cb98a4aaef243e649086b2
Craig Navarro <craignavarro@Craigs-MacBook-Pro.local>
2021-03-08 09:08:15 -0800

    second axios call works

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-const cardSearchURL = `https://api.pokemontcg.io/v2/cards?q=name:charizard`
-const name = 'charizard'
+const setSearchURL = 'https://api.pokemontcg.io/v2/sets'
+const pokemonName = 'charizard'
+const cardSearchURL = `https://api.pokemontcg.io/v2/cards?q=name:`
 const code = '81ef3d57-7b3c-4e91-9eb6-81cb3adbfb06'
 
 
 async function getPokemon() {
   try {
-    const pokemon = await axios.get(`https://api.pokemontcg.io/v2/cards?q=name:charizard`,
+    const pokemon = await axios.get(`${cardSearchURL}${pokemonName}` ,
       {
-      'X-Api-Key': `81ef3d57-7b3c-4e91-9eb6-81cb3adbfb06`
+      'X-Api-Key': `${code}`
     })
     console.log(pokemon.data)
   } catch (error) {
@@ -15,4 +16,16 @@ async function getPokemon() {
   }
 }
 
-getPokemon()
+async function getSet() {
+  try {
+    const sets = await axios.get(`${setSearchURL}` ,
+      {
+      'X-Api-Key': `${code}`
+    })
+    console.log(sets.data.data)
+  } catch (error) {
+    console.log(error.message)
+  }
+}
+
+getSet()
